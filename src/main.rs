23255175b0a44e43c72b5001ff1e91d6e414d1a3
Angel <eliasszust@gmail.com>
2023-02-06 04:58:22 -0300

    output now is prettier

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use console::style;
 use indicatif::{ProgressBar,  style::ProgressStyle};
 use std::{time, thread};
 use clap::Parser;
@@ -17,19 +18,25 @@ struct Arguments{
 }
 
 fn main() {
-
+    
     let args = Arguments::parse();
+
+    //greetings and info about rounds and timing
+    println!("{}\n",
+             style("work - a pomodoro-CLI").green());
+
     //enter the work loop
     for i in 0..args.rounds*2 {
         let bar : ProgressBar;
         if i % 2 == 0 { //work time
-            bar = create_progress_bar("blue", args.work_time, "Time to work");
+            bar = create_progress_bar("blue", args.work_time, "left to work");
         }
         else { //relax time
-            bar = create_progress_bar("yellow", args.relax_time, "Time to relax");
+            bar = create_progress_bar("yellow", args.relax_time, "left to relax");
         }
-        ProgressBar::set_message(&bar, format!("Round number {}", (i/2 + 1)));
+        ProgressBar::set_message(&bar, format!("Phase {}/{}", (i+1), args.rounds*2));
         loop_through_bar(&bar);
+        ProgressBar::finish(&bar);
     }
 
 }
@@ -46,7 +53,7 @@ fn loop_through_bar(bar : &ProgressBar){
 
 fn create_progress_bar(color : &str, duration : u64, message : &str) -> ProgressBar {
     let pb = ProgressBar::new(duration*60);
-    let template = format!("{{msg}}\n[{{elapsed}}] {{bar:40.{}}} {}", color, message);
+    let template = format!("{{msg}} | {{bar:40.{}}} | {{eta}} {}", color, message);
     pb.set_style(ProgressStyle::with_template(&template)
                  .unwrap());
     return pb;
